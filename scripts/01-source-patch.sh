55f96192692037416e4fd4db49af41d91653c9e5
mia0x75 <miao.0x75@gmail.com>
2025-03-28 03:51:40 +0000

    Update script to build branch for standalone

--- scripts/01-source-patch.sh
+++ scripts/01-source-patch.sh
@@ -4,7 +4,4 @@
 cp $GITHUB_WORKSPACE/patches/rustdesk-server-006-fix-webpki-error.patch feeds/packages/net/rustdesk-server/patches/006-fix-webpki-error.patch
 
 patch feeds/packages/lang/golang/golang-build.sh $GITHUB_WORKSPACE/patches/golang-build.sh.patch
-patch feeds/packages/lang/golang/golang/Makefile $GITHUB_WORKSPACE/patches/golang-makefile.patch
-patch feeds/packages/net/ddns-go/Makefile $GITHUB_WORKSPACE/patches/ddns-go-makefile.patch
 patch feeds/packages/net/open-app-filter/Makefile $GITHUB_WORKSPACE/patches/open-app-filter-makefile.patch
-patch feeds/packages/net/sing-box/Makefile $GITHUB_WORKSPACE/patches/sing-box-makefile.patch
