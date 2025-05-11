0e94ce686a693f81a87492c89c51694f056f35a3
mia0x75 <miao.0x75@gmail.com>
2025-05-11 07:24:24 +0000

    Update patch

--- scripts/01-source-patch.sh
+++ scripts/01-source-patch.sh
@@ -4,3 +4,4 @@
 patch feeds/packages/lang/golang/golang-build.sh $GITHUB_WORKSPACE/patches/golang-build.sh.patch
 patch feeds/packages/net/open-app-filter/Makefile $GITHUB_WORKSPACE/patches/open-app-filter-makefile.patch
 patch feeds/packages/lang/rust/rust-host-build.mk $GITHUB_WORKSPACE/patches/rust-host-build.mk.patch
+patch include/target.mk $GITHUB_WORKSPACE/patches/target.mk.patch
